8480a32d12ee7872d3a61d84e50491eed6ff288e
Julie <ssabini.sl@gmail.com>
2021-10-17 13:59:01 +0900

    react router 실습 예제 - react-route-dom 사용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/home';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to = "/">Home</Link>
+        <Link to = "/profile">Profile</Link>
+      </nav>
+      <Switch>
+          <Route path = {["/", "/home"]} exact>
+              <Home ></Home>
+          </Route>
+          <Route path = "/profile">
+              <Profile></Profile>
+          </Route>
+      </Switch>
+    </Router> 
   );
 }
 
